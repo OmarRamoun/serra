eccf7c3bd28e3df5b2f664354a4729ec5f90cd14
MrRamoun <omartarekramoun@gmail.com>
2022-03-19 23:09:04 +0200

    Add regex for input validations

---
+++ src/Validations/regex.js
@@ -0,0 +1,5 @@
+const EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const USERNAME_REGEX = /^[a-zA-Z0-9]{3,30}$/;
+const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,40}$/;
+
+export { EMAIL_REGEX, USERNAME_REGEX, PASSWORD_REGEX };
