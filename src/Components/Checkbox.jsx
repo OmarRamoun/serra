562a57c132ee5e3d61914e38cb72236acbc22efb
MrRamoun <omartarekramoun@gmail.com>
2022-03-19 22:58:16 +0200

    Use Context in checkbox and Field
    Also update Input.jsx and CheckboxContainer.jsx

--- src/Components/Checkbox.jsx
+++ src/Components/Checkbox.jsx
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types'
 
-const Checkbox = ({ id, text, linkText, link }) => {
+const Checkbox = ({ id, text, linkText, link, ...props }) => {
   return (
     <div>
       <div>
-        <input type="checkbox" id={id} />
-        <label htmlfor={id}>
+        <input type="checkbox" id={id} name={id} {...props} />
+        <label htmlFor={id}>
           {text}
           {linkText && <a href={link}>{linkText}</a>}
           .
