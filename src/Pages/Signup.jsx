ff34e51fb8c953890b72921e003b2de37c6ab6f0
MrRamoun <omartarekramoun@gmail.com>
2022-03-20 19:00:49 +0200

    Update Form Fields with accessabibility features and on the fly validation

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -19,40 +19,82 @@ const Signup = () => {
 
 
   return (
-    <Form>
-      <Heading>signup</Heading>
-      <Field
-        autoFocus
-        value={signupForm.username}
-        onChange={handleSignupFormChange}
-      />
-      <Field
-        type="email"
-        value={signupForm.email}
-        onChange={handleSignupFormChange}
-      />
-      <Field
-        type="new-pass"
-        value={signupForm.newPassword}
-        onChange={handleSignupFormChange}
-      />
-      <Field
-        type="confirm-pass"
-        value={signupForm.confirmPassword}
-        onChange={handleSignupFormChange}
-      />
+    <>
+      {
+        success ? ( <h1>Success</h1> )
+          : (
+            <Form onSubmit={handleSubmit}>
+              <p
+                ref={errorMsgRef}
+                className={errMsg ? "err" : "offscreen"}
+                aria-live="assertive"
+              >
+                {errMsg}
+              </p>
+              <Heading>signup</Heading>
+              <Field
+                autoFocus
+                value={signupForm.username.fieldValue}
+                onChange={handleSignupFormChange}
+                aria-invalid={!signupForm.username.valid}
+                aria-describedby="username-error-msg"
+                onFocus={(e) => handleSignupFormFocusChange(e, true)}
+                onBlur={(e) => handleSignupFormFocusChange(e, false)}
+                valid={signupForm.username.valid}
+              />
+              <p
+                id="username-error-msg"
+                className={!signupForm.username.fieldValue && !signupForm.username.valid && signupForm.username.focus ? "offscreen" : "err"}
+                aria-live="assertive"
+              >
+                {<FaInfoCircle />}
+                Username is required and must be between 3 and 20 characters.
+              </p>
+              <Field
+                type="email"
+                value={signupForm.email.fieldValue}
+                onChange={handleSignupFormChange}
+                aria-invalid={!signupForm.email.valid}
+                aria-describedby="email-error-msg"
+                onFocus={(e) => handleSignupFormFocusChange(e, true)}
+                onBlur={(e) => handleSignupFormFocusChange(e, false)}
+                valid={signupForm.email.valid}
+              />
+              <Field
+                type="new-pass"
+                value={signupForm.newPassword.fieldValue}
+                onChange={handleSignupFormChange}
+                aria-invalid={!signupForm.newPassword.valid}
+                aria-describedby="new-password-error-msg"
+                onFocus={(e) => handleSignupFormFocusChange(e, true)}
+                onBlur={(e) => handleSignupFormFocusChange(e, false)}
+                valid={signupForm.newPassword.valid}
+              />
+              <Field
+                type="confirm-pass"
+                value={signupForm.confirmPassword.fieldValue}
+                onChange={handleSignupFormChange}
+                aria-invalid={!signupForm.confirmPassword.valid}
+                aria-describedby="confirm-password-error-msg"
+                onFocus={(e) => handleSignupFormFocusChange(e, true)}
+                onBlur={(e) => handleSignupFormFocusChange(e, false)}
+                valid={signupForm.confirmPassword.valid}
+              />
 
-      <CheckboxContainer />
+              <CheckboxContainer />
 
-      <button
-        type="submit"
-        aria-label="Sign Up"
-      >
-        SIGNUP
-      </button>
-      <HelpLink text="Already Have an Account?" linkText="Login" link="#" />
-    </Form>
-  )
-}
+              <button
+                aria-label="Sign Up"
+                disabled={!signupForm.username.valid || !signupForm.email.valid || !signupForm.newPassword.valid || !signupForm.confirmPassword.valid}
+              >
+                SIGNUP
+              </button>
+              <HelpLink text="Already Have an Account?" linkText="Login" link="#" />
+            </Form>
+          )
+      }
+    </>
+  );
+};
 
 export default Signup;
