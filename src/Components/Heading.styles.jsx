4276553e24993d45f73676dc4f3be2c85d36fbbd
MrRamoun <omartarekramoun@gmail.com>
2022-03-22 16:52:40 +0200

    Refactor Heading comp.
    :heavy_check_mark: rename 'Heading.jsx' into 'Heading.styles.jsx' to anotate that its a (reprsentational) styled comp.
    :heavy_check_mark: remove redundent render function

--- src/Components/Heading.styles.jsx
+++ src/Components/Heading.styles.jsx
@@ -1,18 +1,11 @@
 import styled from 'styled-components';
 
 
-const StyledHeading = styled.h1`
+const Heading = styled.h1`
   font-family: 'Nova Flat', cursive, sans-serif;
   font-size: 2.5rem;
   margin-bottom: 2rem;
+  text-transform: uppercase;
 `;
-
-const Heading = ({ children }) => {
-  return (
-    <StyledHeading>
-      {children.toUpperCase()}
-    </StyledHeading>
-  )
-}
 
 export default Heading;
