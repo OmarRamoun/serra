6987f1ad9d991b11f9b51f3dc0f3d84256b8bc27
MrRamoun <omartarekramoun@gmail.com>
2022-03-24 06:47:28 +0200

    Update ValidationNote comp.
    :heavy_check_mark: add show/hide functionality based on style for the comp.
    :heavy_check_mark: replace structure  with PasswordValidation comp.

--- src/Components/Form/Field/ValidationNote.jsx
+++ src/Components/Form/Field/ValidationNote.jsx
@@ -2,9 +2,12 @@ import { FaInfoCircle } from 'react-icons/fa';
 
 import styled from 'styled-components';
 import PropTypes from 'prop-types'
+import PasswordValidationNote from './PasswordValidation';
 
 
 const Container = styled.div`
+  background: rgba(17, 30, 49, 0.8);
+  padding: 20px;
   display: flex;
   flex: 1;
 `;
@@ -16,7 +19,13 @@ const ValidationNote = ({
   const showNote = focus && value && !valid;
 
   return (
-    <Container>
+    <Container
+      style={
+        showNote ?
+          { position: "relative" } :
+          { position: "relative", left: "-9999px", height: "0px", padding: "0"}
+      }
+    >
       <div
         id={ariaId}
         className={showNote ? "instructions" : "offscreen"}
@@ -46,9 +55,10 @@ const ValidationNote = ({
           (
             <p>
               <FaInfoCircle />
-              8 to 24 characters.<br />
+              {/* 8 to 24 characters.<br />
               Must include uppercase and lowercase letters, a number and a special character.<br />
-              Allowed special characters: <span aria-label="exclamation mark">!</span> <span aria-label="at symbol">@</span> <span aria-label="hashtag">#</span> <span aria-label="dollar sign">$</span> <span aria-label="percent">%</span>
+              Allowed special characters: <span aria-label="exclamation mark">!</span> <span aria-label="at symbol">@</span> <span aria-label="hashtag">#</span> <span aria-label="dollar sign">$</span> <span aria-label="percent">%</span> */}
+              <PasswordValidationNote />
             </p>
           )
         }
@@ -72,7 +82,6 @@ ValidationNote.defaultProps = {
 
 ValidationNote.propTypes = {
   fieldType: PropTypes.string,
-  valid: PropTypes.bool.isRequired,
   ariaId: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
   focus: PropTypes.bool.isRequired,
