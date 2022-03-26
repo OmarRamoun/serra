9ce9cb01ba078078abf8a6fa1eae16231932a331
MrRamoun <omartarekramoun@gmail.com>
2022-03-26 02:56:34 +0200

    Prepare Layout comp. as a parent for (Container & Header)

---
+++ src/Layout.js
@@ -0,0 +1,37 @@
+import Header from "./Components/Header";
+import { FlexColumn } from "./Styles/Flex.styles";
+import bg from "./Assets/Images/bg.jpg";
+
+import styled, { createGlobalStyle } from "styled-components";
+import { Outlet } from "react-router-dom";
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    transition: all 0.3s cubic-bezier(.8,.36,.94,.49);
+  }
+  body {
+    font-family: 'Exo 2', sans-serif;
+  }
+`;
+
+const Container = styled.main`
+  ${FlexColumn}
+  min-height: 100vh;
+  background: url(${bg}) no-repeat fixed;
+  background-size: cover;
+`;
+
+const Layout = () => {
+  return (
+    <Container>
+      <Header />
+      <Outlet />
+      <GlobalStyles />
+    </Container>
+  );
+};
+
+export default Layout;
