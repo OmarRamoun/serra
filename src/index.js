1123720c7c076645d6d881613e847eabd6740f7e
MrRamoun <omartarekramoun@gmail.com>
2022-03-26 02:55:46 +0200

    Wrap App comp. in Router

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import userReducer from "./features/user";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const store = configureStore({
   reducer: {
@@ -15,7 +16,11 @@ const store = configureStore({
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App/>} />
+        </Routes>
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
