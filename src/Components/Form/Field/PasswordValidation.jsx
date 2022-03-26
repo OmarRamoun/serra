5603ff20122a30614648a4ea1ca7cb8050c05320
MrRamoun <omartarekramoun@gmail.com>
2022-03-26 08:40:36 +0200

    Refactor PasswordValidation and ValidationNote Styles

--- src/Components/Form/Field/PasswordValidation.jsx
+++ src/Components/Form/Field/PasswordValidation.jsx
@@ -4,8 +4,25 @@ import { FaCheck, FaTimes } from 'react-icons/fa';
 import styled from 'styled-components';
 
 
-const Container = styled.div`
-  background: rgba(17, 30, 49, 0.8);
+const Container = styled.div``;
+
+const StyledP = styled.p`
+  display: flex;
+  gap: 0.4rem;
+  align-items: flex-start;
+  margin-bottom: 0.5rem;
+
+  > :first-child {
+    margin-top: 0.1rem;
+  }
+`;
+
+const CheckIcon = styled(FaCheck)`
+  color: #00c853;
+`;
+
+const TimesIcon = styled(FaTimes)`
+  color: #f44336;
 `;
 
 const PasswordValidationNote = () => {
@@ -14,36 +31,30 @@ const PasswordValidationNote = () => {
 
   return (
     <Container id="pwdnote">
-      <p>
-        <span>1.</span>
-        {valid.length ? <FaCheck /> : <FaTimes />}
+      <StyledP>
+        {valid.length ? <CheckIcon /> : <TimesIcon />}
         The password must contain at least 8 characters.
-      </p>
-      <p>
-        <span>2.</span>
-        {valid.uppercase ? <FaCheck /> : <FaTimes />}
+      </StyledP>
+      <StyledP>
+        {valid.uppercase ? <CheckIcon /> : <TimesIcon />}
         The password must contain at least 1 uppercase character.
-      </p>
-      <p>
-        <span>3.</span>
-        {valid.lowercase ? <FaCheck /> : <FaTimes />}
+      </StyledP>
+      <StyledP>
+        {valid.lowercase ? <CheckIcon /> : <TimesIcon />}
         The password must contain at least 1 lowercase character.
-      </p>
-      <p>
-        <span>4.</span>
-        {valid.digits ? <FaCheck /> : <FaTimes />}
+      </StyledP>
+      <StyledP>
+        {valid.digits ? <CheckIcon /> : <TimesIcon />}
         The password must contain at least 1 number.
-      </p>
-      <p>
-        <span>5.</span>
-        {valid.specialChar ? <FaCheck /> : <FaTimes />}
+      </StyledP>
+      <StyledP>
+        {valid.specialChar ? <CheckIcon /> : <TimesIcon />}
         The password must contain at least 1 special character.
-      </p>
-      <p>
-        <span>6.</span>
-        {!valid.noSpace ? <FaCheck /> : <FaTimes />}
+      </StyledP>
+      <StyledP>
+        {!valid.noSpace ? <CheckIcon /> : <TimesIcon />}
         The password must not contain any spaces.
-      </p>
+      </StyledP>
     </Container>
   )
 }
