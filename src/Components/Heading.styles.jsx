547899c9c11322877286bacd2c6e926447c18e8d
MrRamoun <omartarekramoun@gmail.com>
2022-03-26 08:32:04 +0200

    Refactor Heading Styles

--- src/Components/Heading.styles.jsx
+++ src/Components/Heading.styles.jsx
@@ -3,9 +3,10 @@ import styled from 'styled-components';
 
 const Heading = styled.h1`
   font-family: 'Nova Flat', cursive, sans-serif;
-  font-size: 2.5rem;
-  margin-bottom: 2rem;
+  font-size: 2.8rem;
+  margin-bottom: 1.6rem;
   text-transform: uppercase;
+  text-shadow: 0 0.4rem 0.3rem rgba(0,0,0,0.3);
 `;
 
 export default Heading;
