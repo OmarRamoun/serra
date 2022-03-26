015c451476d13d7614c44b0a6a2a139d3de3ecf7
MrRamoun <omartarekramoun@gmail.com>
2022-03-26 03:19:20 +0200

    Add conditional rendering for unAuthorized users with an alert box

--- src/Pages/SuccessLogin.jsx
+++ src/Pages/SuccessLogin.jsx
@@ -1,10 +1,45 @@
+import HelpLink from "../Components/HelpLink";
+import Button from "../Components/Button";
+import ErrorBox from "../Components/Form/ErrorBox";
+import { useLogin } from "../Contexts/LoginContext";
+
 import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
 const SuccessLogin = () => {
 
+  const { handleLogout, errMsg, errRef } = useLogin();
+  const { username } = useParams();
   const user = useSelector((state) => state.user.value);
+  console.log("user: ", user);
 
-  return (
+  console.log(handleLogout);
+
+  if (user.username === username) return (
+    <section
+      style={
+        {
+          background: "#fff",
+          padding: "20px",
+          maxWidth: "500px",
+          margin: "220px auto",
+          overflow: "auto"
+        }
+      }
+    >
+      <ErrorBox errMsg={errMsg} errRef={errRef} />
+      <h1>You are logged in!</h1>
+      <br />
+      {
+        Object.entries(user).map(([key, value]) => (
+          <p key={key}>{key}: {value.toString()}</p>
+        ))
+      }
+
+      <Button onClick={handleLogout}>Logout</Button>
+      <HelpLink link="/signup" linkText="Back" text="Go"/>
+    </section>
+  ); else return (
     <section
       style={
         {
@@ -13,16 +48,9 @@ const SuccessLogin = () => {
         }
       }
     >
-      <h1>You are logged in!</h1>
+      <h1>You are not logged in!</h1>
       <br />
-      {
-        Object.entries(user).map(([key, value]) => (
-          <p key={key}>{key}: {value}</p>
-        ))
-      }
-      <p>
-        <a href="#">Go to Home</a>
-      </p>
+      <HelpLink link="/signup" linkText="Back" text="Go"/>
     </section>
   )
 }
