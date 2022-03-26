d82b6f2766d078b358cde40eaa5ae20dea5cbf1c
MrRamoun <omartarekramoun@gmail.com>
2022-03-26 03:26:20 +0200

    Add temporary style to the Button comp.

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,13 +1,43 @@
 import PropTypes from 'prop-types'
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  background: #fff;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  color: #000;
+  font-size: 1.5em;
+  font-weight: bolder;
+  margin: 1em;
+  padding: 0.3em 1.6em;
+  text-decoration: none;
+  transition-duration: 0ms;
+  &:hover {
+    background: #dfdf78;
+    border-color: #999;
+    color: #000;
+  }
+  &:active {
+    background: #eaeaea;
+    border-color: #999;
+    color: #000;
+    transform: translateY(4px) scale(0.95);
+  }
+
+  &:disabled {
+    background: #eee;
+    border-color: #999;
+    color: #999;
+  }
+`;
 
 const Button = ({ children, ...props }) => {
   return (
-    <button
-      
+    <StyledButton
       {...props}
     >
       {children}
-    </button>
+    </StyledButton>
 
   )
 }
